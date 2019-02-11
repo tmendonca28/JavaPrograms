04433f6c53e9ae5b8d159024b5fe32e141fd61ab
tmendonca28 <tmendonca28@gmail.com>
2019-02-11 23:54:12 +0000

    Adds simple variables
    Signed-off-by: tmendonca28 <tmendonca28@gmail.com>

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,5 +1,11 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Anthony!");
+
+        // Variables
+        int myFirstNumber = 10+5;
+        int mySecondNumber = 1000 - myFirstNumber;
+        System.out.println(myFirstNumber);
+        System.out.println(mySecondNumber);
     }
 }
