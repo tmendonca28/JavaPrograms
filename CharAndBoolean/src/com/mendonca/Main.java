7652cd401a784f21b1e208a00d882d0c789dabac
tmendonca28 <tmendonca28@gmail.com>
2019-02-12 16:29:46 +0000

    Add char and boolean implementations
    Signed-off-by: tmendonca28 <tmendonca28@gmail.com>

---
+++ CharAndBoolean/src/com/mendonca/Main.java
@@ -0,0 +1,18 @@
+package com.mendonca;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // Char and Boolean Implementations
+        // char has a size of 16
+        char myChar = '\u00A9';
+        System.out.println("Unicode output was: " + myChar);
+
+        boolean myBoolean = true;
+        boolean isMale = true;
+
+        char isRegisteredSymbol = '\u00AE';
+        System.out.println("Registered symbol is " + isRegisteredSymbol);
+
+    }
+}
