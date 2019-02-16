14d8e50294d467e578f66ee8aeca4372fd15ced1
tmendonca28 <tmendonca28@gmail.com>
2019-02-16 23:24:35 +0000

    Adding simple introduction of operators in Java
    Signed-off-by: tmendonca28 <tmendonca28@gmail.com>

---
+++ Operators/src/com/mendonca/Main.java
@@ -0,0 +1,19 @@
+package com.mendonca;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int result = 1 + 2;
+
+	    int previousResult = result;
+	    result = result - 1;
+        System.out.println(result);
+        System.out.println(previousResult);
+
+        boolean isAlien = false;
+        if (isAlien == true){
+            System.out.println("It is not an alien!");
+        }
+
+    }
+}
