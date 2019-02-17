f7bb73399066a9897c0018990eb64e15b9a59bee
tmendonca28 <tmendonca28@gmail.com>
2019-02-17 18:13:13 +0000

    Adding more operator introduction implementations
    Signed-off-by: tmendonca28 <tmendonca28@gmail.com>

--- Operators/src/com/mendonca/Main.java
+++ Operators/src/com/mendonca/Main.java
@@ -11,8 +11,32 @@ public class Main {
         System.out.println(previousResult);
 
         boolean isAlien = false;
-        if (isAlien == true){
+        if (isAlien){
             System.out.println("It is not an alien!");
+        }
+
+        int newValue = 51;
+        if (newValue == 50) {
+            System.out.println("This is true");
+        } else {
+            System.out.println("This is false");
+        }
+
+        boolean isCar = false;
+
+        // ternary
+        boolean wasCar = isCar ? true : false;
+        if(wasCar){
+            System.out.println("wasCar is true");
+        } else {
+            System.out.println("wasCar is false");
+        }
+
+        double myDouble = 20;
+        double mySecondDouble = 80;
+        double sum = (myDouble + mySecondDouble) * 25;
+        if (sum%40 <= 20) {
+            System.out.println("Total was over the limit");
         }
 
     }
