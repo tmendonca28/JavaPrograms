9fd4abc77313676d22ba23cee6ae676ffb2b07d2
tmendonca28 <tmendonca28@gmail.com>
2020-03-05 21:24:44 +0000

    Learning hashmaps in Java
    Signed-off-by: tmendonca28 <tmendonca28@gmail.com>

---
+++ BasicHashMaps/src/Main.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    static HashMap<String, Boolean> map = new HashMap<>();
+    public static void main(String[] args) {
+        map.put("Anthony", true);
+        map.put("Juliet", false);
+
+        System.out.println(map.get("Juliet"));
+
+        if(map.containsKey("Jack")) {
+            System.out.println(1);
+        }
+        if(map.containsValue(Boolean.FALSE)){
+            System.out.println(2);
+        }
+
+        for(String s: map.keySet()){
+            System.out.println(s);
+        }
+
+        for(Map.Entry kv : map.entrySet()) {
+            System.out.println(kv);
+        }
+
+        map.remove("Anthony");
+
+        System.out.println(map);
+    }
+}
