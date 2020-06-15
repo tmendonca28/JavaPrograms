d09dc76dea8f784df86ada1d8283533463846a19
tmendonca28 <tmendonca28@gmail.com>
2020-06-16 00:06:34 +0100

    Adds PlayingCat.java
    Implementation to check whether the cat is still playing.
    The cat plays when temp is between 25 and 35 (inclusive).
    Upper limit changes to 45 when it is summer.
    Signed-off-by: tmendonca28 <tmendonca28@gmail.com>

---
+++ Playing Cat/src/com/mendonca/Main.java
@@ -0,0 +1,11 @@
+package com.mendonca;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println(PlayingCat.isCatPlaying(true, 10));
+        System.out.println(PlayingCat.isCatPlaying(false, 36));
+        System.out.println(PlayingCat.isCatPlaying(false, 35));
+    }
+}
