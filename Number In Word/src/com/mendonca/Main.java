41976433b9a3afbc53f5dd63092354d6c5cc0a68
tmendonca28 <tmendonca28@gmail.com>
2020-06-16 23:22:06 +0100

    Adds NumberInWord Challenge
    Practising switch statements in Java.
    Signed-off-by: tmendonca28 <tmendonca28@gmail.com>

---
+++ Number In Word/src/com/mendonca/Main.java
@@ -0,0 +1,9 @@
+package com.mendonca;
+
+public class Main {
+
+    public static void main(String[] args) {
+        NumberInWord.printNumberInWord(3);
+        NumberInWord.printNumberInWord(12);
+    }
+}
