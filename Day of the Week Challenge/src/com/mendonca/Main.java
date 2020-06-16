9f8fde88229a16a83a375406e98441fecf783b43
tmendonca28 <tmendonca28@gmail.com>
2020-06-16 23:14:49 +0100

    Adds DayOfTheWeekChallenge
    Simple challenge to practise switch statements in Java.
    Signed-off-by: tmendonca28 <tmendonca28@gmail.com>

---
+++ Day of the Week Challenge/src/com/mendonca/Main.java
@@ -0,0 +1,10 @@
+package com.mendonca;
+
+public class Main {
+
+    public static void main(String[] args) {
+        DayOfTheWeek.printDayOfTheWeek(4);
+        DayOfTheWeek.printDayOfTheWeek(42);
+
+    }
+}
