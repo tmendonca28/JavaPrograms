f813c56a4d037cab9e5b749fd974f98bdde22a73
tmendonca28 <tmendonca28@gmail.com>
2020-06-17 23:07:37 +0100

    Adds simple while implementation
    Signed-off-by: tmendonca28 <tmendonca28@gmail.com>

---
+++ While Statement/src/com/mendonca/Main.java
@@ -0,0 +1,12 @@
+package com.mendonca;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int count = 1;
+        while (count != 6) {
+            System.out.println("Count value is " + count);
+            count++;
+        }
+    }
+}
