5b62e351d2b05413cff07ecadda90a4e515d0c34
tmendonca28 <tmendonca28@gmail.com>
2020-06-21 12:12:09 +0100

    Learning about the parse method
    Signed-off-by: tmendonca28 <tmendonca28@gmail.com>

---
+++ Parsing Values From String/src/com/mendonca/Main.java
@@ -0,0 +1,18 @@
+package com.mendonca;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String numberAsString = "2018";
+        System.out.println("numberAsString " + numberAsString);
+
+        int number = Integer.parseInt(numberAsString);
+        System.out.println("number = " + number);
+
+        numberAsString += 1;
+        number += 1;
+
+        System.out.println(numberAsString); // Appends
+        System.out.println(number);         // Adds
+    }
+}
