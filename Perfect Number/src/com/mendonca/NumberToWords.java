c1c897400b54774ba8f0d40436ee1a20809ce693
tmendonca28 <tmendonca28@gmail.com>
2020-06-24 23:26:02 +0100

    Adds Number To Words program
    Signed-off-by: tmendonca28 <tmendonca28@gmail.com>

---
+++ Perfect Number/src/com/mendonca/NumberToWords.java
@@ -0,0 +1,15 @@
+package com.mendonca;
+
+public class NumberToWords {
+    public static void numberToWords (int number) {
+        if (number < 0) {
+            System.out.println("Invalid Value");
+        }
+        if (number == 0) {
+            System.out.println("Zero");
+        }
+        while (number > 0) {
+
+        }
+    }
+}
