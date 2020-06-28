313d531a9e31842a896c4bae097457a3c5af0ce6
tmendonca28 <tmendonca28@gmail.com>
2020-06-28 16:15:16 +0100

    Adds Classes project
    Cementing basics on creating classes in java.
    State vs Behavior.
    Signed-off-by: tmendonca28 <tmendonca28@gmail.com>

---
+++ Classes/src/com/mendonca/Car.java
@@ -0,0 +1,12 @@
+package com.mendonca;
+
+public class Car {
+
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String color;
+
+
+}
---
+++ Classes/src/com/mendonca/Main.java
@@ -0,0 +1,10 @@
+package com.mendonca;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Car bmw = new Car();
+        Car toyota = new Car();
+        bmw.
+    }
+}
