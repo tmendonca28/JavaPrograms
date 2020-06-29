a8325d7a177bf65c41c58f560e2bb873ee989e18
tmendonca28 <tmendonca28@gmail.com>
2020-06-29 18:21:50 +0100

    Adds check
    Adds a check in setModel method.
    Allows to check for certain criteria before setting state.
    Signed-off-by: tmendonca28 <tmendonca28@gmail.com>

--- Classes/src/com/mendonca/Car.java
+++ Classes/src/com/mendonca/Car.java
@@ -8,5 +8,17 @@ public class Car {
     private String engine;
     private String color;
 
+    public void setModel(String model) {
+        // this is field specific
+        String validModel = model.toLowerCase();
+        if(validModel.equals("m3") || validModel.equals("commodore")) {
+            this.model = model;
+        } else {
+            this.model = "Unknown";
+        }
+    }
 
+    public String getModel() {
+        return this.model;
+    }
 }
